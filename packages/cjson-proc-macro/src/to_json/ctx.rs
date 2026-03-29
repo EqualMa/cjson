27854a2bc3c4d00066b2bc4ed51f957529dc6c16
Equal Ma <equalma@outlook.com>
2026-03-30 02:16:36 +0800

    chore: refactor ContextWithFields::expand_self

--- packages/cjson-proc-macro/src/to_json/ctx.rs
+++ packages/cjson-proc-macro/src/to_json/ctx.rs
@@ -672,6 +672,10 @@ impl context_with_fields::ContextWithFields for ContextOfStruct {
     fn fields(&self) -> &[StructField] {
         &self.fields
     }
+
+    fn fields_ident_to_index(&self) -> Option<&std::collections::HashMap<String, usize>> {
+        self.fields_ident_to_index.as_ref()
+    }
 }
 
 impl bracket_star::ContextSupportsAtBracketStar for ContextOfStruct {
@@ -1221,49 +1225,6 @@ impl ContextOfStruct {
             ]
             .into()
         })
-    }
-
-    fn expand_self(
-        &mut self,
-        span_self: Span,
-        rest_prop: Vec<expand_props::Prop>,
-        out: TokensCollector<'_>,
-        errors: &mut ErrorCollector,
-    ) {
-        let mut rest_prop = rest_prop.into_iter();
-
-        let Some(field_name_or_index) = rest_prop.next() else {
-            errors.push_custom("@self on struct cannot expand to tokens", span_self);
-            return;
-        };
-
-        let span = field_name_or_index.span();
-
-        let index = match &field_name_or_index {
-            expand_props::Prop::Ident(ident) => {
-                self.fields_ident_to_index
-                    .as_ref()
-                    .and_then(|fields_ident_to_index| {
-                        fields_ident_to_index.get(&ident.to_string()).copied()
-                    })
-            }
-            expand_props::Prop::Literal(literal) => usize::from_str_radix(&literal.to_string(), 10)
-                .ok()
-                .and_then(|i| if i < self.fields.len() { Some(i) } else { None }),
-        };
-
-        let Some(index) = index else {
-            errors.push_custom("field doesn't exist in struct", span);
-            return;
-        };
-
-        ContextOfStructField {
-            ctx_struct: self,
-            index_field: index,
-            span,
-            span_self: Some(span_self),
-        }
-        .expand_field_props_maybe_empty(rest_prop, out, errors)
     }
 }
 
--- packages/cjson-proc-macro/src/to_json/ctx/context_with_fields.rs
+++ packages/cjson-proc-macro/src/to_json/ctx/context_with_fields.rs
@@ -1,11 +1,16 @@
 use proc_macro::Span;
 
-use crate::to_json::ctx::field::ContextSupportsField;
+use crate::{
+    ErrorCollector,
+    expand_props::{self, TokensCollector},
+    to_json::ctx::field::ContextSupportsField,
+};
 
 use super::{StructField, field::ContextOfField};
 
 pub trait ContextWithFields: ContextSupportsField {
     fn fields(&self) -> &[StructField];
+    fn fields_ident_to_index(&self) -> Option<&std::collections::HashMap<String, usize>>;
 
     fn for_each_non_skip_field(
         &mut self,
@@ -30,10 +35,62 @@ pub trait ContextWithFields: ContextSupportsField {
             };
         }
     }
+
+    fn expand_self(
+        &mut self,
+        span_self: Span,
+        rest_prop: Vec<expand_props::Prop>,
+        out: TokensCollector<'_>,
+        errors: &mut ErrorCollector,
+    ) {
+        let mut rest_prop = rest_prop.into_iter();
+
+        let Some(field_name_or_index) = rest_prop.next() else {
+            errors.push_custom("@self cannot expand to tokens", span_self);
+            return;
+        };
+
+        let span = field_name_or_index.span();
+
+        let index = match &field_name_or_index {
+            expand_props::Prop::Ident(ident) => {
+                self.fields_ident_to_index()
+                    .and_then(|fields_ident_to_index| {
+                        fields_ident_to_index.get(&ident.to_string()).copied()
+                    })
+            }
+            expand_props::Prop::Literal(literal) => usize::from_str_radix(&literal.to_string(), 10)
+                .ok()
+                .and_then(|i| {
+                    if i < self.fields().len() {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                }),
+        };
+
+        let Some(index) = index else {
+            errors.push_custom("field doesn't exist in struct", span);
+            return;
+        };
+
+        ContextOfField {
+            ctx_struct: self,
+            index_field: index,
+            span,
+            span_self: Some(span_self),
+        }
+        .expand_field_props_maybe_empty(rest_prop, out, errors)
+    }
 }
 
 impl<Ctx: ?Sized + ContextWithFields> ContextWithFields for &mut Ctx {
     fn fields(&self) -> &[StructField] {
         Ctx::fields(self)
+    }
+
+    fn fields_ident_to_index(&self) -> Option<&std::collections::HashMap<String, usize>> {
+        Ctx::fields_ident_to_index(self)
     }
 }
