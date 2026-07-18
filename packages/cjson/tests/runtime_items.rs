948270d7dc7267a11c844fc74ee706808d6c8872
Equal Ma <equalma@outlook.com>
2026-07-18 19:44:30 +0800

    chore: tests for runtime items

--- packages/cjson/tests/runtime_items.rs
+++ packages/cjson/tests/runtime_items.rs
@@ -1,11 +1,20 @@
 use cjson::{
-    impl_to_json, json,
-    ser::{ToJson, ToJsonArray, exts::TextExt as _},
+    impl_to_json,
+    ser::{ToJson2 as ToJson, ToJsonArray2 as ToJsonArray},
 };
 
 macro_rules! assert_json_eq {
-    ($v:expr,$eq:expr) => {
-        assert_eq!($v.to_json().into_string().into_inner(), $eq)
+    ($v:expr, $eq:expr) => {
+        assert_eq!(::cjson::ser::ToJsonExt::to_json_as_string(&$v), $eq)
+    };
+}
+
+macro_rules! json {
+    ([]) => {
+        [] as [u8; 0]
+    };
+    ($e:expr) => {
+        $e
     };
 }
 
@@ -16,7 +25,7 @@ impl_to_json!(
     where_clause![where T: ToJsonArray],
     |self: BetweenBrackets<T>| [
         //
-        ..(&self.0) as &'cjson_lt_to_json T
+        ..(&self.0) as &'_ T
     ],
 );
 
@@ -24,33 +33,8 @@ struct BetweenBracketsChained<T1: ToJsonArray, T2: ToJsonArray>(T1, T2);
 
 impl_to_json!(
     impl_generics![T1: ToJsonArray, T2: ToJsonArray],
-    |self: BetweenBracketsChained<T1, T2>| [
-        ..(&self.0) as &'cjson_lt_to_json T1,
-        ..(&self.1) as &'cjson_lt_to_json T2,
-    ],
+    |self: BetweenBracketsChained<T1, T2>| [..(&self.0) as &'_ T1, ..(&self.1) as &'_ T2,],
 );
-
-impl<T: ToJsonArray> ToJsonArray for BetweenBrackets<T> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
-impl<T1: ToJsonArray, T2: ToJsonArray> ToJsonArray for BetweenBracketsChained<T1, T2> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
 
 #[test]
 fn between_brackets() {
@@ -80,7 +64,7 @@ impl_to_json!(
     impl_generics![T: ToJsonArray],
     |self: AfterArrayStartBeforeItem<T>| [
         //
-        ..(&self.0) as &'cjson_lt_to_json T,
+        ..(&self.0) as &'_ T,
         null,
     ],
 );
@@ -89,33 +73,11 @@ impl_to_json!(
     impl_generics![A: ToJsonArray, B: ToJsonArray],
     |self: AfterArrayStartBeforeItemChained<A, B>| [
         //
-        ..(&self.0) as &'cjson_lt_to_json A,
-        ..(&self.1) as &'cjson_lt_to_json B,
+        ..(&self.0) as &'_ A,
+        ..(&self.1) as &'_ B,
         null,
     ],
 );
-
-impl<T: ToJsonArray> ToJsonArray for AfterArrayStartBeforeItem<T> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
-impl<T1: ToJsonArray, T2: ToJsonArray> ToJsonArray for AfterArrayStartBeforeItemChained<T1, T2> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
 
 #[test]
 fn after_array_start_before_item() {
@@ -144,36 +106,12 @@ fn after_array_start_before_item() {
 pub struct AfterItemBeforeBracket<T: ToJsonArray>(T);
 pub struct AfterItemBeforeBracketChained<V: ToJson, A: ToJsonArray, B: ToJsonArray>(V, A, B);
 
-impl<T: ToJsonArray> ToJsonArray for AfterItemBeforeBracket<T> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
-impl<V: ToJson, T1: ToJsonArray, T2: ToJsonArray> ToJsonArray
-    for AfterItemBeforeBracketChained<V, T1, T2>
-{
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
 impl_to_json!(
     impl_generics![T: ToJsonArray],
     |self: AfterItemBeforeBracket<T>| [
         //
         false,
-        ..(&self.0) as &'cjson_lt_to_json T,
+        ..(&self.0) as &'_ T,
     ],
 );
 
@@ -181,9 +119,9 @@ impl_to_json!(
     impl_generics![V: ToJson, A: ToJsonArray, B: ToJsonArray],
     |self: AfterItemBeforeBracketChained<V, A, B>| [
         //
-        (&self.0) as &'cjson_lt_to_json V,
-        ..(&self.1) as &'cjson_lt_to_json A,
-        ..(&self.2) as &'cjson_lt_to_json B,
+        (&self.0) as &'_ V,
+        ..(&self.1) as &'_ A,
+        ..(&self.2) as &'_ B,
     ],
 );
 
@@ -214,36 +152,12 @@ fn after_item_before_bracket() {
 pub struct AfterItemBeforeItem<T: ToJsonArray>(T);
 pub struct AfterItemBeforeItemChained<A: ToJsonArray, B: ToJsonArray, V: ToJson>(A, B, V);
 
-impl<T: ToJsonArray> ToJsonArray for AfterItemBeforeItem<T> {
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
-impl<T1: ToJsonArray, T2: ToJsonArray, V: ToJson> ToJsonArray
-    for AfterItemBeforeItemChained<T1, T2, V>
-{
-    type ToJsonArray<'a>
-        = <Self as ToJson>::ToJson<'a>
-    where
-        Self: 'a;
-
-    fn to_json_array(&self) -> Self::ToJsonArray<'_> {
-        <Self as ToJson>::to_json(self)
-    }
-}
-
 impl_to_json!(
     impl_generics![T: ToJsonArray],
     |self: AfterItemBeforeItem<T>| [
         //
         false,
-        ..(&self.0) as &'cjson_lt_to_json T,
+        ..(&self.0) as &'_ T,
         1u8,
     ],
 );
@@ -253,9 +167,9 @@ impl_to_json!(
     |self: AfterItemBeforeItemChained<A, B, V>| [
         //
         true,
-        ..(&self.0) as &'cjson_lt_to_json A,
-        ..(&self.1) as &'cjson_lt_to_json B,
-        (&self.2) as &'cjson_lt_to_json V,
+        ..(&self.0) as &'_ A,
+        ..(&self.1) as &'_ B,
+        (&self.2) as &'_ V,
     ],
 );
 
