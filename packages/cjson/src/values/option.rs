ce96bd1835308cd9c958940e04523613786ff38b
Equal Ma <equalma@outlook.com>
2026-08-05 01:57:32 +0800

    feat: impl Into/ToJson for Option

--- packages/cjson/src/values/option.rs
+++ packages/cjson/src/values/option.rs
@@ -1,18 +1,37 @@
-#![cfg(todo)] // impl IntoJson for Option
-use crate::ser::{IntoJson, ToJson2};
+use crate::ser::{IntoJson, ToJson2 as ToJson, helpers::json_fns, json_kinds};
 
-use super::{Either, Null};
+use super::Null;
 
 impl<T: ToJson> ToJson for Option<T> {
-    type ToJson<'a>
-        = Either<T::ToJson<'a>, Null>
-    where
-        Self: 'a;
-
-    fn to_json(&self) -> Self::ToJson<'_> {
-        match self {
-            Some(this) => Either::A(T::to_json(this)),
-            None => Either::B(Null),
+    type ToJsonKind = json_kinds::AnyValue;
+    const IS_CHAINABLE_AND_ALWAYS_EMPTY: bool = false;
+    json_fns!({
+        json_provide_to::json_provide_to_try::json_provide_to_async_try::ToJsonKind;
+        use trait_mod;
+        async |&self, w| {
+            use trait_mod::{XHelpers as _, await_};
+            if let Some(this) = self {
+                await_!(w.consume_any_value_of_any_kind(this, ()))
+            } else {
+                await_!(Null.json_provide_into_x(w))
+            }
         }
-    }
+    });
+}
+
+impl<T: IntoJson> IntoJson for Option<T> {
+    type JsonKind = json_kinds::AnyValue;
+    const IS_CHAINABLE_AND_ALWAYS_EMPTY: bool = false;
+    json_fns!({
+        json_provide_into::json_provide_into_try::json_provide_into_async_try::JsonKind;
+        use trait_mod;
+        async |self, w| {
+            use trait_mod::{XHelpers as _, await_};
+            if let Some(this) = self {
+                await_!(w.consume_any_value_of_any_kind(this, ()))
+            } else {
+                await_!(Null.json_provide_into_x(w))
+            }
+        }
+    });
 }
